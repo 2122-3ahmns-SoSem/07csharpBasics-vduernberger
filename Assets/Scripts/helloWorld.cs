fd1f3061d007ba64f8b52786999e86a04dc7f077
vduernberger <100835083+vduernberger@users.noreply.github.com>
2022-03-10 10:16:46 +0100

    Add files via upload
    First Upload HelloWorld-Project

---
+++ Assets/Scripts/helloWorld.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour
+{
+    public void HelloWorld()
+    {
+        Debug.Log("Hello World");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        HelloWorld();
+    }
+
+}
