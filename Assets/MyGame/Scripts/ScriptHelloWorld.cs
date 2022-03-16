ea61a4761a6bcfad04330ac21e0784bad56713ee
vduernberger <100835083+vduernberger@users.noreply.github.com>
2022-03-16 10:51:43 +0100

    Add files via upload
    Hello World - Upload 2
    Änderungen:
    - MyGame Ordner
    - Nomenklatur

---
+++ Assets/MyGame/Scripts/ScriptHelloWorld.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ScriptHelloWorld : MonoBehaviour
+{
+    public void HelloWorld()
+    {
+        Debug.Log("Hello World");
+    }
+ 
+    void Start()
+    {
+        HelloWorld();
+    }
+
+}
---
+++ Assets/MyGame/Scripts/Variablen.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public enum Himmelsrichtungen
+{
+    Nord,
+    Sued,
+    West,
+    Ost
+}
+
+public class Variablen : MonoBehaviour
+{
+    Color farbe;
+    indexer anzTueren;
+    bool fahren;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Himmelsrichtungen.Nord + " " + Himmelsrichtungen.Sued + " " + Himmelsrichtungen.West + " " + Himmelsrichtungen.Ost);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
