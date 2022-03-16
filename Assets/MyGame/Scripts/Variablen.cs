48f8efd674e77c9455584f6bbf592ffb2d5efbfd
vduernberger <100835083+vduernberger@users.noreply.github.com>
2022-03-16 11:29:46 +0100

    Variablen Aufgabe
    Upload 01 der Hausaufgabe zum Thema Variablen.

--- Assets/MyGame/Scripts/Variablen.cs
+++ Assets/MyGame/Scripts/Variablen.cs
@@ -11,18 +11,36 @@ public enum Himmelsrichtungen
 public class Variablen : MonoBehaviour
 {
     Color farbe;
-    indexer anzTueren;
+    int anzTueren;
+    int anzTueren2;
+    int anzTueren3;
     bool fahren;
+    string name;
+
+    //Ich habe gemeinsam Laura Forsthuber und Sophe Kloiber gearbeitet :)
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(Himmelsrichtungen.Nord + " " + Himmelsrichtungen.Sued + " " + Himmelsrichtungen.West + " " + Himmelsrichtungen.Ost);
+
+        //man kann beispielsweise Variablen nutzen, um bestimmte Personen anzusprechen:
+        name = "Frau Meerwald-Stadler"; 
+        Debug.Log("Hallo " + name);
+
+        //eine weitere Anwendungsfunktion wäre beispielsweise, um Werte zu berechnen:
+        anzTueren = 5;
+        anzTueren2 = 6;
+        anzTueren3 = 7;
+        Debug.Log(anzTueren + anzTueren2 + anzTueren3);
+
+        //man kann durch Variablem auch die Farbe von Objekten ändern:
+        farbe = Color.red;
+        Debug.Log(farbe);
+        gameObject.GetComponent<Renderer>().material.color = farbe;
+
+        //Man kann auch Werte vergleichen lassen, also hier beispielsweise ob anzTueren größer ist als anzTueren2
+        Debug.Log(anzTueren < anzTueren2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
